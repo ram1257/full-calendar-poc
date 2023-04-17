5a5b900f0ce7f70728d1b47aff2643053964b600
Ramachandran <ramachandran.p@optisolbusiness.com>
2023-04-17 17:26:08 +0530

    Removed console

--- src/components/notification/NotificationComponent.jsx
+++ src/components/notification/NotificationComponent.jsx
@@ -1,7 +1,6 @@
 import { useState, useEffect } from "react";
 
 function NotificationComponent({ props }) {
-  console.log(props);
   const [notificationGranted, setNotificationGranted] = useState(false);
 
   useEffect(() => {
